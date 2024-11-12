e1b3dd9ea1f51edf18939418c9cf3fdd74287a96
Cyro Paulino da Costa Jr <cyropcjr@gmail.com>
2024-11-12 10:06:54 -0300

    Add: Add Instrumental Test checking all workers.
    update: Update gradle dependencies library also compileSDK to 34 to fix issue related with dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ plugins {
 
 android {
     namespace = "com.example.bluromatic"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.bluromatic"
@@ -67,22 +67,29 @@ android {
 
 dependencies {
 
-    implementation(platform("androidx.compose:compose-bom:2023.10.00"))
-    implementation("androidx.activity:activity-compose:1.8.0")
-    implementation("androidx.activity:activity-ktx:1.8.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation(platform("androidx.compose:compose-bom:2024.10.01"))
+    implementation("androidx.activity:activity-compose:1.9.3")
+    implementation("androidx.activity:activity-ktx:1.9.3")
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.runtime:runtime")
     implementation("androidx.compose.runtime:runtime-livedata")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:${rootProject.extra["lifecycle_version"]}")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:${rootProject.extra["lifecycle_version"]}")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:${rootProject.extra["lifecycle_version"]}")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:${rootProject.extra["lifecycle_version"]}")
-    implementation("androidx.work:work-runtime-ktx:2.8.1")
+    implementation("androidx.work:work-runtime-ktx:2.10.0")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
+
+    // Espresso
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    // Junit
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    // Work testing
+    androidTestImplementation("androidx.work:work-testing:2.10.0")
 }
